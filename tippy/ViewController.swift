8b0f79a14d9cbb0bb6885bd60f4e4ac864c0ce0d
Shreyansh Gandhi <sgandhi@walmartlabs.com>
2016-04-27 19:32:07 -0700

    First commit

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.billField.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func calulateTip(sender: AnyObject) {
+        
+        let tipPercentages = [0.18, 0.2, 0.25]
+        let bill  = NSString(string: self.billField.text).doubleValue
+        println("bill = ", bill)
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // NOT WORKING BELOW
+        //let bill = Double(billField.text!) ?? 0
+        
 
+        //tipLabel.text = "$\(tip)"
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        //print("hello")
+        view.endEditing(true)
+    }
 }
 
